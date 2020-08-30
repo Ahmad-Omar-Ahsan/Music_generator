a69f8142da63865bf1802d17c579e5f9a0e1000a
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2020-08-31 02:09:45 +0600

    disabled eager execution

--- train.py
+++ train.py
@@ -3,14 +3,14 @@
 import sys, random, os
 import numpy as np
 from matplotlib import pyplot as plt
-import pydot
 import cv2
 import util
 import midi
 import os
 import math
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Activation, Dropout, Flatten, Reshape, Permute, RepeatVector, ActivityRegularization, TimeDistributed, Lambda, SpatialDropout1D, Conv1D, Conv2D, Conv2DTranspose, UpSampling2D, ZeroPadding2D, Embedding,MaxPooling2D, AveragePooling2D,LocallyConnected2D, GaussianNoise, BatchNormalization, LSTM, SimpleRNN
+from tensorflow.keras.layers import  Dense, Activation, Dropout, Flatten, Reshape, Permute, RepeatVector, ActivityRegularization, TimeDistributed, Lambda, SpatialDropout1D, Conv1D, Conv2D, Conv2DTranspose, UpSampling2D, ZeroPadding2D, Embedding,MaxPooling2D, AveragePooling2D,LocallyConnected2D, GaussianNoise, BatchNormalization, LSTM, SimpleRNN
+from tensorflow.keras import Input
 from tensorflow.keras.initializers import RandomNormal
 from tensorflow.keras.losses import binary_crossentropy
 from tensorflow.keras.models import Model, Sequential, load_model
@@ -18,6 +18,7 @@ from tensorflow.keras.optimizers import Adam, RMSprop, SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.utils import plot_model
+tf.compat.v1.disable_eager_execution()
 
 
 NUM_EPOCHS = 2000
@@ -141,168 +142,175 @@ if WRITE_HISTORY:
     if not os.path.exists('History'):
         os.makedirs('History')
 
-with tf.device('/device:GPU:0'):
-    print('Loading data')
-    y_samples = np.load('samples.npy')
-    y_lengths = np.load('lengths.npy')
-    num_samples = y_samples.shape[0]
-    num_songs = y_lengths.shape[0]
-    print("Loaded {} samples from {} songs".format(num_samples, num_songs))
-    print(np.sum(y_lengths))
-    assert(np.sum(y_lengths) == num_samples)
 
-    print("Padding songs")
-    x_shape = (num_songs* NUM_OFFSETS, 1)
-    y_shape = (num_songs * NUM_OFFSETS, MAX_LENGTH) + y_samples.shape[1:]
-    x_orig = np.expand_dims(np.arange(x_shape[0]), axis=-1)
-    y_orig = np.zeros(y_shape, dtype=y_samples.dtype)
-    cur_ix = 0
+print('Loading data')
+y_samples = np.load('samples.npy')
+y_lengths = np.load('lengths.npy')
+num_samples = y_samples.shape[0]
+num_songs = y_lengths.shape[0]
+print("Loaded {} samples from {} songs".format(num_samples, num_songs))
+print(np.sum(y_lengths))
+assert(np.sum(y_lengths) == num_samples)
 
-    for i in range(num_songs):
-        for ofs in range(NUM_OFFSETS):
-            ix  = i * NUM_OFFSETS + ofs
-            end_ix = cur_ix + y_lengths[i]
-            for j in range(MAX_LENGTH):
-                k = (j + ofs) % (end_ix - cur_ix)
-                y_orig[ix, j] = y_samples[cur_ix + k]
-        cur_ix = end_ix
+print("Padding songs")
+x_shape = (num_songs* NUM_OFFSETS, 1)
+y_shape = (num_songs * NUM_OFFSETS, MAX_LENGTH) + y_samples.shape[1:]
+x_orig = np.expand_dims(np.arange(x_shape[0]), axis=-1)
+y_orig = np.zeros(y_shape, dtype=y_samples.dtype)
+cur_ix = 0
 
-    assert(end_ix == num_samples)
-    x_train = np.copy(x_orig)
-    y_train = np.copy(y_orig)
+for i in range(num_songs):
+    for ofs in range(NUM_OFFSETS):
+        ix  = i * NUM_OFFSETS + ofs
+        end_ix = cur_ix + y_lengths[i]
+        for j in range(MAX_LENGTH):
+            k = (j + ofs) % (end_ix - cur_ix)
+            y_orig[ix, j] = y_samples[cur_ix + k]
+    cur_ix = end_ix
+
+assert(end_ix == num_samples)
+x_train = np.copy(x_orig)
+y_train = np.copy(y_orig)
 
 
 
-    test_ix = 0
-    y_test_song = np.copy(y_train[test_ix:test_ix+1])
-    x_test_song = np.copy(x_train[test_ix:test_ix+1])
-    midi.samples_to_midi(y_test_song[0], 'gt.mid', 16)
+test_ix = 0
+y_test_song = np.copy(y_train[test_ix:test_ix+1])
+x_test_song = np.copy(x_train[test_ix:test_ix+1])
+midi.samples_to_midi(y_test_song[0], 'gt.mid', 16)
 
-    if CONTINUE_TRAIN or PLAY_ONLY:
-        print("Loading model")
-        model = load_model('model.h5', custom_objects=custom_objects)
+if CONTINUE_TRAIN or PLAY_ONLY:
+    print("Loading model")
+    model = load_model('model.h5', custom_objects=custom_objects)
 
+else:
+    print("Building model")
+    
+    if USE_EMBEDDING:
+        x_in = Input(shape=x_shape[1:])
+        print((None, ) + x_shape[1:])
+        x = Embedding(x_train.shape[0], PARAM_SIZE, input_length=1)(x_in)
+        x = Flatten(name='pre_encoder')(x)
+    
     else:
-        print("Building model")
-        
-        if USE_EMBEDDING:
-            x_in = Input(shape=x_shape[1:])
-            print((None, ) + x_shape[1:])
-            x = Reshape((y_shape[1], -1))(x_in)
+        x_in = Input(shape=y_shape[1:])
+        print((None, ) + x_shape[1:])
+        x = Reshape((y_shape[1], -1))(x_in)
 
-            x = TimeDistributed(Dense(2000, activation='relu'))(x)
-            x = TimeDistributed(Dense(200, activation='relu'))(x)
-            x = Flatten()(x)
-            x = Dense(1600, activation='relu')(x)
 
-            if USE_VAE:
-                z_mean = Dense(PARAM_SIZE)(x)
-                z_log_sigma_sq = Dense(PARAM_SIZE)(x)
-                x = Lambda(vae_sampling, output_shape=(PARAM_SIZE,), name='pre_encoder')([z_mean, z_log_sigma_sq])
-            else:
-                x = Dense(PARAM_SIZE)(x)
-                x = BatchNormalization(momentum=BN_M, name='pre_encoder')(x)
-
-        x = Dense(1600, name='encoder')(x)
-        x = BatchNormalization(momentum=BN_M)(x)
-        x = Activation('relu')(x)
-        if DO_RATE > 0:
-            x = Dropout(DO_RATE)(x)
-        x = Dense(MAX_LENGTH * 200)(x)
-        x = Reshape((MAX_LENGTH, 200))(x)
-        x = TimeDistributed(BatchNormalization(momentum=BN_M))(x)
-        x = Activation('relu')(x)
-        if DO_RATE > 0:
-            x = Dropout(DO_RATE)(x)
-            x = TimeDistributed(Dense(2000))(x)
-        x = TimeDistributed(BatchNormalization(momentum=BN_M))(x)
-        x = Activation('relu')(x)
-        if DO_RATE > 0:
-            x = Dropout(DO_RATE)(x)
-        x = TimeDistributed(Dense(y_shape[2] * y_shape[3], activation='sigmoid'))(x)
-        x = Reshape((y_shape[1], y_shape[2], y_shape[3]))(x)
+        x = TimeDistributed(Dense(2000, activation='relu'))(x)
+        x = TimeDistributed(Dense(200, activation='relu'))(x)
+        x = Flatten()(x)
+        x = Dense(1600, activation='relu')(x)
 
         if USE_VAE:
-            model = Model(x_in, x)
-            model.compile(optimizer=Adam(lr=LR), loss=vae_loss)
+            z_mean = Dense(PARAM_SIZE)(x)
+            z_log_sigma_sq = Dense(PARAM_SIZE)(x)
+            x = Lambda(vae_sampling, output_shape=(PARAM_SIZE,), name='pre_encoder')([z_mean, z_log_sigma_sq])
         else:
-            model = Model(x_in, x)
-            model.compile(optimizer = RMSprop(lr = LR), loss='binary_crossentropy')
-        
-        plot_model(model, to_file='model.png', show_shapes=True)
+            x = Dense(PARAM_SIZE)(x)
+            x = BatchNormalization(momentum=BN_M, name='pre_encoder')(x)
+
+    x = Dense(1600, name='encoder')(x)
+    x = BatchNormalization(momentum=BN_M)(x)
+    x = Activation('relu')(x)
+    if DO_RATE > 0:
+        x = Dropout(DO_RATE)(x)
+    x = Dense(MAX_LENGTH * 200)(x)
+    x = Reshape((MAX_LENGTH, 200))(x)
+    x = TimeDistributed(BatchNormalization(momentum=BN_M))(x)
+    x = Activation('relu')(x)
+    if DO_RATE > 0:
+        x = Dropout(DO_RATE)(x)
+        x = TimeDistributed(Dense(2000))(x)
+    x = TimeDistributed(BatchNormalization(momentum=BN_M))(x)
+    x = Activation('relu')(x)
+    if DO_RATE > 0:
+        x = Dropout(DO_RATE)(x)
+    x = TimeDistributed(Dense(y_shape[2] * y_shape[3], activation='sigmoid'))(x)
+    x = Reshape((y_shape[1], y_shape[2], y_shape[3]))(x)
+
+    if USE_VAE:
+        model = Model(x_in, x)
+        model.compile(optimizer=Adam(lr=LR), loss=vae_loss)
+    else:
+        model = Model(x_in, x)
+        model.compile(optimizer = RMSprop(lr = LR), loss='binary_crossentropy')
+    
+    plot_model(model, to_file='model.png', show_shapes=True)
 
 
-    print("Compiling submodels")
-    func = tf.keras.backend.function([model.get_layer('encoder').input, tf.keras.backend.learning_phase()],
-                    [model.layers[-1].output])
+print("Compiling submodels")
+func = tf.keras.backend.function([model.get_layer('encoder').input, tf.keras.backend.learning_phase()],
+                [model.layers[-1].output])
 
-    enc = Model(inputs=model.input, outputs=model.get_layer('pre_encoder').output)
+enc = Model(inputs=model.input, outputs=model.get_layer('pre_encoder').output)
 
-    rand_vecs = np.random.normal(0.0, 1.0, (NUM_RAND_SONGS, PARAM_SIZE))
-    np.save('rand.npy', rand_vecs)
-
-
-    if PLAY_ONLY:
-        print("Generating Songs...")
-        make_rand_songs_normalized('', rand_vecs)
-        for i in range(20):
-            x_test_song = x_train[i:i+1]
-            y_song = model.predict(x_test_song, batch_size=BATCH_SIZE)[0]
-            midi.samples_to_midi(y_song, 'gt' + str(i) + '.mid', 16)
-        exit(0)
+rand_vecs = np.random.normal(0.0, 1.0, (NUM_RAND_SONGS, PARAM_SIZE))
+np.save('rand.npy', rand_vecs)
 
 
-    print("Training...")
-    save_config()
-    train_loss = []
-    ofs = 0
+if PLAY_ONLY:
+    print("Generating Songs...")
+    make_rand_songs_normalized('', rand_vecs)
+    for i in range(20):
+        x_test_song = x_train[i:i+1]
+        y_song = model.predict(x_test_song, batch_size=BATCH_SIZE)[0]
+        midi.samples_to_midi(y_song, 'gt' + str(i) + '.mid', 16)
+    exit(0)
 
-    for iter in range(NUM_EPOCHS):
-        if USE_EMBEDDING:
-            history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs = 1)
-        else:
-            cur_ix = 0
-            for i in range(num_songs):
-                end_ix = cur_ix = y_lengths[i]
-                for j in range(MAX_LENGTH):
-                    k = (j + ofs) % (end_ix - cur_ix)
-                    y_train[i,j] = y_samples[cur_ix + k]
-                cur_ix = end_ix
-            assert(end_ix == num_samples)
-            ofs += 1
 
-            history = model.fit(y_train, y_train, batch_size=BATCH_SIZE, epochs=1)
-        
-        loss = history.history["loss"][-1]
-        train_loss.append(loss)
-        print("Train Loss: {}".format(train_loss[-1]))
+print("Training...")
+save_config()
+train_loss = []
+ofs = 0
 
+for iter in range(NUM_EPOCHS):
+    if USE_EMBEDDING:
+        history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs = 1)
+    else:
+        cur_ix = 0
+        for i in range(num_songs):
+            end_ix = cur_ix = y_lengths[i]
+            for j in range(MAX_LENGTH):
+                k = (j + ofs) % (end_ix - cur_ix)
+                y_train[i,j] = y_samples[cur_ix + k]
+            cur_ix = end_ix
+        assert(end_ix == num_samples)
+        ofs += 1
+
+        history = model.fit(y_train, y_train, batch_size=BATCH_SIZE, epochs=1)
+    
+    loss = history.history["loss"][-1]
+    train_loss.append(loss)
+    print("Train Loss: {}".format(train_loss[-1]))
+
+    if WRITE_HISTORY:
+        plot_scores(train_loss, 'History/Scores.png', True)
+    else:
+        plot_scores(train_loss, "Scores.png", True)
+    i = iter + 1
+
+    if i in [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 250, 300, 350, 400, 450] or (i % 100 == 0):
+        write_dir = ''
         if WRITE_HISTORY:
-            plot_scores(train_loss, 'History/Scores.png', True)
+            write_dir = 'History/e' + str(i)
+            if not os.path.exists(write_dir):
+                os.makedirs(write_dir)
+            write_dir += '/'
+            model.save("History/model.h5")
         else:
-            plot_scores(train_loss, "Scores.png", True)
-        i = iter + 1
+            model.save('model.h5')
+        print("saved")
+        if USE_EMBEDDING:
+            y_song = model.predict(x_test_song, batch_size=BATCH_SIZE)[0]
+        else:
+            y_song = model.predict(y_test_song, batch_size=BATCH_SIZE)[0]
+        util.samples_to_pics(write_dir + 'test', y_song)
+        midi.samples_to_midi(y_song, write_dir + 'test.mid', 16)
+        make_rand_songs_normalized(write_dir, rand_vecs)
 
-        if i in [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 250, 300, 350, 400, 450] or (i % 100 == 0):
-            write_dir = ''
-            if WRITE_HISTORY:
-                write_dir = 'History/e' + str(i)
-                if not os.path.exists(write_dir):
-                    os.makedirs(write_dir)
-                write_dir += '/'
-                model.save("History/model.h5")
-            else:
-                model.save('model.h5')
-            print("saved")
-            if USE_EMBEDDING:
-                y_song = model.predict(x_test_song, batch_size=BATCH_SIZE)[0]
-            else:
-                y_song = model.predict(y_test_song, batch_size=BATCH_SIZE)[0]
-            util.samples_to_pics(write_dir + 'test', y_song)
-            midi.samples_to_midi(y_song, write_dir + 'test.mid', 16)
-            make_rand_songs_normalized(write_dir, rand_vecs)
-
-    print("Done")
+print("Done")
 
 
 
